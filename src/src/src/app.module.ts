145f10cb526094dfcd11cdcf14baa231eba96b1f
Samuel Luna <92549469+gsgluna@users.noreply.github.com>
2023-09-19 11:11:09 -0300

    Delete src/src/src/app.module.ts

--- src/src/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AwsModule } from './aws/aws.module';
-
-@Module({
-  imports: [ConfigModule.forRoot(), AwsModule],
-})
-export class AppModule {}
