3b1bc1125b88e528313d82e3a869869de689f5c2
Samuel Luna <92549469+gsgluna@users.noreply.github.com>
2023-09-20 13:44:47 -0300

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AwsModule } from './aws/aws.module';
+import { AwsController } from './aws/aws.controller';
+import { AwsService } from './aws/aws.service';
+
 
 @Module({
   imports: [ConfigModule.forRoot(), AwsModule],
+  controllers: [AwsController],
+  providers: [AwsService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(3000);
 
-  logger.debug(`🚀 Server is running on port: ${port}`);
+  logger.debug(`?? Server is running on port: ${port}`);
 }
 
 bootstrap();
