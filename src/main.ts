9eb8aca6a6b077e6cb3ab9cbf2a833e194c828ca
Samuel Luna <92549469+gsgluna@users.noreply.github.com>
2023-09-20 13:57:40 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 
   logger.debug(`?? Server is running on port: ${port}`);
 }
